fdf74ee57d9d47951ab7a563cc58ff11e0aabf24
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2023-11-02 20:59:53 +0200

    added new section

---
+++ src/js/main.ts
@@ -0,0 +1,45 @@
+import Swiper from 'swiper';
+
+import 'swiper/css';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper(".mainSlider", {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    slidesPerGroup: 1,
+    centeredSlides: true,
+    loop: true,
+    speed: 700,
+
+    modules: [Navigation, Pagination],
+    pagination: {
+        el: '.pagination',
+    },
+
+    navigation: {
+        nextEl: '.next',
+        prevEl: '.prev',
+    },
+
+    breakpoints: {
+
+        320: {
+            slidesPerView: 1.5,
+            spaceBetween: 50,
+        },
+
+        480: {
+            slidesPerView: 2.2,
+            spaceBetween: 60,
+        },
+
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 60,
+        },
+
+        1400: {
+            spaceBetween: 40,
+        }
+    }
+});
