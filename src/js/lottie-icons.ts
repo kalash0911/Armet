acbfbf1fd59d8a9c432a21428990e26a4f73256a
yzhura <zhuravlev.yaroslav@gmail.com>
2023-11-03 17:11:20 +0200

    fix after merge

--- src/js/lottie-icons.ts
+++ src/js/lottie-icons.ts
@@ -45,41 +45,10 @@ document.addEventListener('DOMContentLoaded', async () => {
       }
     });
 
-    const animations = animationsData.map((animationData) => {
-      const iconContainer = document.getElementById(
-        animationData.elementId,
-      ) as HTMLDivElement;
-
-      if (iconContainer) {
-        const anim = lottie.loadAnimation({
-          container: iconContainer,
-          renderer: 'svg',
-          loop: true,
-          autoplay: false,
-          path: animationData.jsonPath,
-          name: animationData.animationName,
-        });
-
-        return anim;
+    itemCard.addEventListener('mouseleave', () => {
+      if (animations[index]) {
+        animations?.[index]?.stop();
       }
-
-      return null;
-    });
-
-    const itemCards = document.querySelectorAll('.item-card');
-
-    itemCards.forEach((itemCard, index) => {
-      itemCard.addEventListener('mouseenter', () => {
-        if (animations[index]) {
-          animations?.[index]?.play();
-        }
-      });
-
-      itemCard.addEventListener('mouseleave', () => {
-        if (animations[index]) {
-          animations?.[index]?.stop();
-        }
-      });
     });
   });
 });
